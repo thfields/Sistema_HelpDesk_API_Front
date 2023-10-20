f9ee76d7e52b31d295a6f8b55b75cc470389d2df
Thiago Campos <=>
2023-10-20 15:39:03 -0300

    componente tecnico-read

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './views/components/home/home.component';
+import { TecnicoReadComponent } from './views/components/tecnico/tecnico-read/tecnico-read.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'tecnicos',
+    component: TecnicoReadComponent
+  },
+  {
+    path: 'clientes',
+    component: TecnicoReadComponent
+  },
+  {
+    path: 'os',
+    component: TecnicoReadComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,13 +22,17 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
 import { HeaderComponent } from './views/components/template/header/header.component';
 import { FooterComponent } from './views/components/template/footer/footer.component';
 import { NavComponent } from './views/components/template/nav/nav.component';
+import { HomeComponent } from './views/components/home/home.component';
+import { TecnicoReadComponent } from './views/components/tecnico/tecnico-read/tecnico-read.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent,
+    TecnicoReadComponent
   ],
   imports: [
     BrowserModule, 
