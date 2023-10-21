ad17569f7dbbb1884a4df84e6ae0438ae526c35e
Thiago Campos <=>
2023-10-21 02:48:00 -0300

    validação do formulario do tecnico

--- src/app/views/components/tecnico/tecnico-create/tecnico-create.component.ts
+++ src/app/views/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { FormControl, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
 import { Tecnico } from "src/app/models/tecnico";
 import { TecnicoService } from "src/app/services/tecnico.service";
@@ -11,10 +12,14 @@ import { TecnicoService } from "src/app/services/tecnico.service";
 export class TecnicoCreateComponent implements OnInit {
   tecnico: Tecnico = {
     id: "",
-    nome: "Rodrigo",
-    cpf: "557.940.060-80",
-    telefone: "84 92222-2222",
+    nome: "",
+    cpf: "",
+    telefone: "",
   };
+
+  nome = new FormControl("", [Validators.minLength(2)]);
+  cpf = new FormControl("", [Validators.minLength(11)]);
+  telefone = new FormControl("", [Validators.minLength(11)]);
 
   constructor(private router: Router, private service: TecnicoService) {}
 
@@ -28,13 +33,39 @@ export class TecnicoCreateComponent implements OnInit {
     this.service.create(this.tecnico).subscribe(
       (resposta) => {
         this.router.navigate(["tecnicos"]);
-        this.service.message("Tecnico criado com sucesso!");
+        this.service.message("Técnico criado com sucesso!");
       },
       (err) => {
         if (err.error.error.match("já cadastrado")) {
           this.service.message(err.error.error);
+        } else if (
+          err.error.erros[0].message ===
+          "número do registro de contribuinte individual brasileiro (CPF) inválido"
+        ) {
+          this.service.message("CPF inválido!");
         }
       }
     );
+  }
+
+  errorValidName() {
+    if (this.nome.invalid) {
+      return "O nome deve ter entre 2 a 100 caracteres";
+    }
+    return false;
+  }
+
+  errorValidCpf() {
+    if (this.cpf.invalid) {
+      return "O CPF deve ter 11 dígitos";
+    }
+    return false;
+  }
+
+  errorValidTel() {
+    if (this.telefone.invalid) {
+      return "O telefone precisa ter o DDD + 9 dígitos";
+    }
+    return false;
   }
 }
