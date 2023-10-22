cdf11bdb5f372de3dcb898531fbd8e06461a2185
Thiago Campos <=>
2023-10-22 18:50:27 -0300

    criando os - construção

--- src/app/views/components/os/os-create/os-create.component.ts
+++ src/app/views/components/os/os-create/os-create.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Cliente } from 'src/app/models/cliente';
+import { Tecnico } from 'src/app/models/tecnico';
+import { ClienteService } from 'src/app/services/cliente.service';
+import { TecnicoService } from 'src/app/services/tecnico.service';
 
 @Component({
   selector: 'app-os-create',
@@ -8,9 +13,36 @@ import { Component, OnInit } from '@angular/core';
 export class OsCreateComponent  implements OnInit{
   selected = ''
 
-  constructor(){}
+  prioridade = ''
+
+  tecnicos: Tecnico[] = []
+
+  clientes: Cliente[] = []
+
+  constructor(
+    private tecnicoService: TecnicoService,
+    private clienteService: ClienteService,
+    private route: ActivatedRoute
+  ){}
 
   ngOnInit(): void {
-      
+    this.listarTecnicos();
+    this.listarClientes();
   }
+
+  listarTecnicos():void{
+    this.tecnicoService.findAll().subscribe(resposta => {
+      this.tecnicos = resposta
+    })
+  }
+
+  listarClientes(): void{
+    this.clienteService.findAll().subscribe(resposta => {
+      this.clientes = resposta
+    })
+  }
+
+  
+  
+
 }
